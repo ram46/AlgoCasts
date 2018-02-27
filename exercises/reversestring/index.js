cfc3fddd48288b69537310be2ac523add9d210c0
Khizra <khiz@pwr.local>
2018-02-26 22:01:04 -0800

    reversestrig done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce(function(acc, char) { return char + acc }, '');
+}
+
+// Method 2 //
+function reverse2(str) {
+  var result = '';
+  for (var i = 0; i < str.length; i++) {
+    result = str[i] + result;
+  }
+	return result;
+}
+
+// Method 3 //
+function reverse(str) {
+  var result = '';
+  for (var i = str.length - 1; i >= 0; i--) {
+    result = result + str[i];
+  }
+  
+  return result;
+}
+
 
 module.exports = reverse;
