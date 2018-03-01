aedba63eb207563288cbe03072faeb5ca0e7f22a
Khizra <khiz@pwr.local>
2018-02-28 23:43:45 -0800

    maxchar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+// method 1
+// function maxChar(str) {
+// 	var max = 0;
+// 	var mostFreqChar = '';
+// 	var charObj = str.split('').reduce(function(acc, char) { acc[char] = acc[char]+1 || 1 ; return acc }, {})
+// 	for (var char in charObj) {
+// 		if (charObj[char] > max) {
+// 			max = charObj[char]
+// 			mostFreqChar = char;
+// 		}
+// 	}
+// 	return mostFreqChar;
+// }
+
+
+// method 2
+function maxChar(str) {
+	var charObj = str.split('').reduce(function(acc, char) { acc[char] = acc[char]+1 || 1 ; return acc }, {})
+	return Object.keys(charObj).filter(function(char) { 
+		return charObj[char] === Math.max(...Object.values(charObj)) 
+	}).join('')
+}
 
 module.exports = maxChar;
+
+
